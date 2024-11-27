2efd11d0bcfe5076d2278508372c4025909a462e
Josh Cusick <josh.m.cusick@gmail.com>
2024-11-26 20:34:16 -0500

    Updated to accept dynamic properties

--- src/app/data/componentMap.js
+++ src/app/data/componentMap.js
@@ -4,6 +4,7 @@ export const componentMap = [
     // The parent of key of the component, regardless of the variant
     // e.g. node.mainComponent.parent.key
     oldParentKey: "db7764b6b623f78d703a479fff35a7d622cdcd8e",
+
     // The new component that is imported and swapped in, this MUST be specific to the variant you want!
     // e.g. node.mainComponent.key
     newComponentKey: "09a1beeac1f5dcec7e138455d43a6d6c2dfaecea",
@@ -12,5 +13,5 @@ export const componentMap = [
     groupName: "Button Group",
     oldParentKey: "060df2468891d0bade4669f57affbf43eec41e69",
     newComponentKey: "3564264c80c78a57babe1d6a64a66aa0a51807bc",
-  }
-]
+  },
+];
